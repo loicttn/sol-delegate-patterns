ce07a81ce877a3599abc159f765559a53c3c4f55
pwnh4 (@loicttn) <loic@ttn.am>
2023-05-13 13:17:46 +0200

    feat: slot doc

--- src/libs/Storage.sol
+++ src/libs/Storage.sol
@@ -1,6 +1,48 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+/** THIS IS A STUPID, INCOMPLETE AND MOST PROBABLY UNSAFE IMPLEM, DO NOT USE.
+
+    Evm slots are 256 bits long and are used to store contract variables.
+
+    A practice often used with delegatecall patterns, is to used unstructured storage
+    to store variables. This is done by using the keccak256 hash of the variable name
+    as the slot key. This prevents storage collisions between different contracts.
+
+    Some standards are defined like EIP1967 to define some standard slots to store
+    proxy configs for example.
+    
+    This library provides a way to store and retrieve values in evm slots.
+
+    Example of a delegate call storage being modified by an implem.
+
+    +-----------------+ .           +-----------------+
+    | Proxy           |             | TargetedImplem  |
+    |                 |             |                 |
+    |                 |  delegate   |                 |
+    |  Slot 1         | ----------> |                 |
+    |  +-----------+  |   calls     |                 |
+    |  |  Address  |  |             |     Logic       |
+    |  +-----------+  |             |  write aa to s1 |
+    | Slot 2          |             |  write 42 to s4 |
+    |  +-----------+  |             |  s4 += 1        |
+    |  |  Uint256  |  |             |  ...            |
+    |  +-----------+  |             |                 |
+    |                 |             |                 |
+    | ...             |             |                 |
+    |                 |             |                 |
+    | Slot 43         |             |                 |
+    |  +-----------+  |             |                 |
+    |  |  Uint256  |  |             |                 |
+    |  +-----------+  |             |                 |
+    | Slot 44         |             |                 |
+    |  +-----------+  |             |                 |
+    |  |  Uint256  |  |             |                 |
+    |  +-----------+  |             |                 |
+    |                 |             |                 |
+    +-----------------+ .           +-----------------+
+
+*/
 library Storage {
     ////////////////////////
     // ADDRESS
