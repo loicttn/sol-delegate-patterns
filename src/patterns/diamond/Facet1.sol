9b1ef93a357e5d9e6030df583ab7d2dcdce6ab44
pwnh4 (@loicttn) <loic@ttn.am>
2023-05-15 10:47:06 +0200

    feat: add diamond pattern

---
+++ src/patterns/diamond/Facet1.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.8.13;
+
+import "../../libs/Storage.sol";
+
+contract DiamondFacet1 {
+    modifier onlyOnce() {
+        bytes32 init_slot = bytes32(
+            uint256(keccak256("facet1.initialized")) - 1
+        );
+        bool init = Storage.getBool(init_slot).v;
+        require(!init, "already initialized");
+        Storage.setBool(init_slot, true);
+        _;
+    }
+
+    // this must be called only once
+    function initialize() public onlyOnce {
+        Storage.setUint256(
+            bytes32(uint256(keccak256("facet1.number")) - 1),
+            42
+        );
+    }
+
+    // incrments a uint256 stored in a shared facet storage
+    function increment_shared() public {
+        Storage.Uint256 storage nb = Storage.getUint256(
+            bytes32(uint256(keccak256("shared.number")) - 1)
+        );
+        nb.v++;
+    }
+
+    // increments a uint256 stored in a dedicated facet storage
+    function increment() public {
+        Storage.Uint256 storage nb = Storage.getUint256(
+            bytes32(uint256(keccak256("facet1.number")) - 1)
+        );
+        nb.v++;
+    }
+
+    function number_1() public view returns (uint256) {
+        return
+            Storage
+                .getUint256(bytes32(uint256(keccak256("facet1.number")) - 1))
+                .v;
+    }
+}
